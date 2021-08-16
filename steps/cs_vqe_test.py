24c40ee59ee0de38f234e4418ad7979357b13054
Tim <timothy.weaving.20@ucl.ac.uk>
2021-08-16 02:08:35 +0100

    underscore

--- steps/cs_vqe_test.py
+++ steps/cs_vqe_test.py
@@ -1,7 +1,7 @@
 print('Importing...')
-import CS-VQE.cs_vqe_classes.cs_vqe_circuit as cs_circ
-import CS-VQE.utils.cs_vqe_tools as cs_tools
-import CS-VQE.utils.molecule_tools as mol
+import CS_VQE.cs_vqe_classes.cs_vqe_circuit as cs_circ
+import CS_VQE.utils.cs_vqe_tools as cs_tools
+import CS_VQE.utils.molecule_tools as mol
 print('Finished imports')
 def cs_vqe_test(bond_len, multiplicity, charge, basis, rot_A, num_sim_q, atom1=None, atom2=None, atom3=None):
     """
